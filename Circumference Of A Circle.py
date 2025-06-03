9abe445d76836c9a98235212486eae072feef04d
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-03 18:24:23 -0500

    Delete Circumference Of A Circle.py

--- Circumference Of A Circle.py
+++
@@ -1,9 +0,0 @@
-# Circumference of a circle
-# C=2πr
-
-import math
-
-radius = float(input(f"What is the radius of the circle?: "))
-circumference = 2 * math.pi * radius
-
-print(f"The circumference of the circle is: {round(circumference, 2)}cm")
