50f73f34b19922313938a5b7e39b41456913bb91
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-03 18:26:02 -0500

    Delete Shopping Cart Program.py

--- Shopping Cart Program.py
+++
@@ -1,8 +0,0 @@
-# Shopping Cart Program
-
-item = input("What item are your buying?: ")
-price = float(input("What is the price of the item shown?: "))
-quantity = int(input("How many of the items do you have?: "))
-total = (price * quantity)
-
-print(f"The total amount is: ${total}")
