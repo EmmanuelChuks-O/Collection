6dbdb059e70a1550f452c3c248bdc4627e29e7c4
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-03 21:15:07 -0400

    Add files via upload

---
+++ Dictionary 1.py
@@ -0,0 +1,18 @@
+# dictionary = a collection of {key:value} pairs
+#              ordered and changeable. No duplicates
+
+capitals = {"USA": "Washington D.C",
+            "France": "Paris",
+            "Japan": "Tokyo",
+            "United Kingdom": "London",
+            "Canada": "Ottawa"}
+
+# print(dir(capitals))
+# print(help(capitals))
+# print(capitals.get("Canada"))
+# capitals.update({"Germany": "Berlin"})
+# capitals.pop("United Kingdom")
+# capitals.clear()
+# keys = capitals.keys()
+# values = capitals.values()
+print(capitals)
