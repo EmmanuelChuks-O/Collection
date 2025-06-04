ad0c0fbc65c23f673d86393fd8339b5491a2d1e8
Emmanuel Chuks Okoh <okoh.ce@gmail.com>
2025-06-03 21:18:39 -0400

    Add files via upload

---
+++ Dictionary 2.py
@@ -0,0 +1,10 @@
+# Welcome to Dictionaries
+
+students = {"Emmanuel": 29, "Oiza": 28, "Oluchi": 31}
+print(students)
+print(students["Emmanuel"])
+students["Oiza"] = 29 # updating Oiza's age
+del students["Oluchi"] # delete
+print(students["Oiza"])
+print(students)
+print(len(students))
---
+++ Dictonary 3.py
@@ -0,0 +1,29 @@
+# dictionary
+
+# syntax
+
+# dict = {key1:value1, key2:value2, key3:value3....}
+
+my_dict = {'name': 'Emmanuel', 'age': '29', 'city': 'Toronto'}
+print(my_dict)
+
+# dictionary operations
+
+# access dictionary key values
+
+print(my_dict['name'])
+
+# add new key value pairs
+
+my_dict['job'] = "Machine Learning Engineer"
+print(my_dict)
+
+# remove a key value pair
+
+del my_dict['age']
+print(my_dict)
+
+# replace value of a key
+
+my_dict['city'] = 'Etobicoke'
+print(my_dict)
